5050a4a32b8121d2245fe6f9494f7ccf0a742159
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-08 07:15:10 +1000

    Fix env vars names

--- kong-dashboard/docker-entrypoint.sh
+++ kong-dashboard/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/dumb-init /bin/bash
+#!/bin/bash
 set -e
 
 # usage: file_env VAR [DEFAULT]
@@ -24,22 +24,18 @@ file_env() {
 	unset "$fileVar"
 }
 
-file_env 'KONG_DASHBOARD_ADMIN_NAME'
+file_env 'KONG_DASHBOARD_NAME'
 
-if [[ -z "$KONG_DASHBOARD_ADMIN_NAME" ]]; then
-  echo >&2 'You need to specify one of KONG_DASHBOARD_ADMIN_NAME or KONG_DASHBOARD_ADMIN_NAME_FILE'
+if [[ -z "$KONG_DASHBOARD_NAME" ]]; then
+  echo >&2 'You need to specify one of KONG_DASHBOARD_NAME or KONG_DASHBOARD_NAME_FILE'
   exit 1
 fi
 
-export kong-dashboard-name="$KONG_DASHBOARD_ADMIN_NAME"
+file_env 'KONG_DASHBOARD_PASS'
 
-file_env 'KONG_DASHBOARD_ADMIN_PASSWORD'
-
-if [[ -z "$KONG_DASHBOARD_ADMIN_PASSWORD" ]]; then
-  echo >&2 'You need to specify one of KONG_DASHBOARD_ADMIN_PASSWORD or KONG_DASHBOARD_ADMIN_PASSWORD_FILE'
+if [[ -z "$KONG_DASHBOARD_PASS" ]]; then
+  echo >&2 'You need to specify one of KONG_DASHBOARD_PASS or KONG_DASHBOARD_PASS_FILE'
   exit 1
 fi
-
-export kong-dashboard-pass="$KONG_DASHBOARD_ADMIN_PASSWORD"
 
 npm run start -- $@
