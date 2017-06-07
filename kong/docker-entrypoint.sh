5670c5bfcbba38d8228222645c516f65a6618fb1
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-08 00:23:24 +1000

    [kong] Add file_env for KONG_PG_DATABASE and KONG_PG_USER

--- kong/docker-entrypoint.sh
+++ kong/docker-entrypoint.sh
@@ -24,6 +24,20 @@ file_env() {
 	unset "$fileVar"
 }
 
+file_env 'KONG_PG_DATABASE'
+
+if [[ -z "$KONG_PG_DATABASE" ]]; then
+  echo >&2 'You need to specify one of KONG_PG_DATABASE or KONG_PG_DATABASE_FILE'
+  exit 1
+fi
+
+file_env 'KONG_PG_USER'
+
+if [[ -z "$KONG_PG_USER" ]]; then
+  echo >&2 'You need to specify one of KONG_PG_USER or KONG_PG_USER_FILE'
+  exit 1
+fi
+
 file_env 'KONG_PG_PASSWORD'
 
 if [[ -z "$KONG_PG_PASSWORD" ]]; then
