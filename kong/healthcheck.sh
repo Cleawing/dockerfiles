f8139fc89ad7e6982d950f2912200f64878fd596
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-08 20:45:49 +1000

    [kong] Add HEALTCHECK inside Dockerfile

--- kong/healthcheck.sh
+++ kong/healthcheck.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 
-nc -w 5 $KONG_PG_HOST $KONG_PG_PORT < /dev/null && curl -s -f http://localhost:8001/cluster > /dev/null
+nc -w 5 $KONG_PG_HOST $KONG_PG_PORT < /dev/null && \
+  nc -w 5 localhost 8000 </dev/null && \
+  curl -s -f http://localhost:8001/cluster > /dev/null
